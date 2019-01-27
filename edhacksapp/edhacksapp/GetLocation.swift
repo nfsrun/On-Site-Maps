d282b59918e54c11318639a81e2dd3e17be69ed8
mohariz <m.rizkyhariz@yahoo.com>
2019-01-27 12:54:00 -0800

    bleh

--- edhacksapp/edhacksapp/GetLocation.swift
+++ edhacksapp/edhacksapp/GetLocation.swift
@@ -14,24 +14,33 @@ class GetLocation: UIViewController, CLLocationManagerDelegate, UIPickerViewDele
 
     let locationManager = CLLocationManager()
     let client = MSClient(applicationURLString: "https://edhacksapp.azurewebsites.net")
+    var pickerData: [String] = ["accounting", "airport","amusement park","aquarium", "art gallery","atm","bakery","bank","bar","beauty salon","bicycle store","book store","bowling alley","bus station","cafe","campground","car dealer","car rental","car repair","car wash","casino","cemetery","church","city hall","clothing store","convenience store","courthouse","dentist","department store","doctor","electrician","electronics store","embassy","fire station","florist","funeral home","furniture store","gas station","gym", "hair_care","hardware store","hindu temple","home goods store","hospital", "insurance agency","jewelry store","laundry","lawyer","library","liquor store","local government_office","locksmith","lodging","meal delivery","meal takeaway","mosque","movie rental","movie theater","moving company","museum", "night club", "painter","park","parking","pet store","pharmacy", "physiotherapist","plumber","police","post office","real estate agency", "restaurant", "roofing contractor", "rv park","school","shoe store","shopping mall","spa","stadium","storage","store","subway station","supermarket", "synagogue","taxi stand", "train station", "transit station", "travel agency","veterinary care","zoo"]
+    
     @IBOutlet weak var locationDial: UIPickerView!
-    var pickerData: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self as! CLLocationManagerDelegate
         locationManager.requestWhenInUseAuthorization()
          //Do any additional setup after loading the view.
-        self.locationDial.delegate = self as! UIPickerViewDelegate
-        self.locationDial.dataSource = self as! UIPickerViewDataSource
+        
+//        self.locationDial.delegate = self as UIPickerViewDelegate
+//        self.locationDial.dataSource = self as! UIPickerViewDataSource
+//        
+//        
         
         pickerData = ["accounting", "airport","amusement park","aquarium", "art gallery","atm","bakery","bank","bar","beauty salon","bicycle store","book store","bowling alley","bus station","cafe","campground","car dealer","car rental","car repair","car wash","casino","cemetery","church","city hall","clothing store","convenience store","courthouse","dentist","department store","doctor","electrician","electronics store","embassy","fire station","florist","funeral home","furniture store","gas station","gym", "hair_care","hardware store","hindu temple","home goods store","hospital", "insurance agency","jewelry store","laundry","lawyer","library","liquor store","local government_office","locksmith","lodging","meal delivery","meal takeaway","mosque","movie rental","movie theater","moving company","museum", "night club", "painter","park","parking","pet store","pharmacy", "physiotherapist","plumber","police","post office","real estate agency", "restaurant", "roofing contractor", "rv park","school","shoe store","shopping mall","spa","stadium","storage","store","subway station","supermarket", "synagogue","taxi stand", "train station", "transit station", "travel agency","veterinary care","zoo"]
+        
+
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Number of columns of data
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 1
     }
@@ -55,38 +64,28 @@ class GetLocation: UIViewController, CLLocationManagerDelegate, UIPickerViewDele
             
             print("stop 1")
             let tblObject = client.table(withName: "tblObject")
-            print("stop 2")
-            let tblObjectType = client.table(withName: "TodoItem")
-
-            tblObjectType.read { (result, error) in
-                if let err = error {
-                    print("ERROR ", err)
-                } else if let items = result?.items {
-                    for item in items {
-                        print("Todo Item: ", item["text"])
-                    }
-                }
-            }
-//
-//            tblObject.read { (result, error) in
+            let tblCoordinate = client.table(withName: "tblCoordinate")
+            
+//            var qResult: Int = 0
+//            let query = tblObject.query()
+//            query.selectFields = ["locationTypeID", "complete"]
+//            query.parameters = ["locationTypeName": "accounting"]
+//            query.read { (result, error) in
 //                if let err = error {
 //                    print("ERROR ", err)
 //                } else if let items = result?.items {
 //                    for item in items {
-//                        print("Todo Item: ", item["text"])
+//                        qResult = item
 //                    }
 //                }
 //            }
-//
-//            let tblCoordinate = client.table(withName: "dbo.tblCoordinate")
-//            print("stop 3")
 
 
             let newObject = ["objectTypeID": 1, "locationTypeID": 1]
             print("stop 4")
 
-//            let newCoordinate = ["objectID": 1, "long": long, "lat": lat]
-//            print("stop 5")
+            let newCoordinate = ["objectID": 1,"long": long, "lat": lat]
+            print("stop 5")
 
 
             tblObject.insert(newObject) { (result, error) in
@@ -94,16 +93,15 @@ class GetLocation: UIViewController, CLLocationManagerDelegate, UIPickerViewDele
                     
                     print("ERROR ", err)
                 } else if let item = result {
-                    print("Todo Item: ", item["text"]!)
+                    //print("Todo Item: ", item["text"]!)
                 }
             }
-//            tblCoordinate.insert(newCoordinate) { (result, error) in
-//                if let err = error {
-//                    print("ERROR ", err)
-//                } else if let item = result {
-//                    print("Todo Item: ", item["text"]!)
-//                }
-//            }
+            tblCoordinate.insert(newCoordinate) { (result, error) in
+                if let err = error {
+                    print("ERROR ", err)
+                } else if let item = result {
+                }
+            }
 //
             
             
@@ -118,14 +116,10 @@ class GetLocation: UIViewController, CLLocationManagerDelegate, UIPickerViewDele
     
     @IBOutlet weak var locationTypeName: UITextField!
     @IBAction func getGPSLocation(_ sender: Any) {
-        locationManager.requestLocation()
-        print(locationTypeName.text)
         
+        locationManager.requestLocation()
     }
     
 }
-//extension ViewController: CLLocationManagerDelegate {
-//
-//}
 
 
